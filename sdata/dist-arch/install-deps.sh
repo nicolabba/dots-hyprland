a1963a52914e16466a63a76c5ddcf326f8c52538
nicolabba <nicola.abbagnato@gmail.com>
2026-08-05 23:52:31 +0200

    Add denuvowo kernel module installation

--- sdata/dist-arch/install-deps.sh
+++ sdata/dist-arch/install-deps.sh
@@ -95,6 +95,7 @@ metapkgs+=(./sdata/dist-arch/illogical-impulse-hyprland)
 metapkgs+=(./sdata/dist-arch/illogical-impulse-microtex-git)
 metapkgs+=(./sdata/dist-arch/illogical-impulse-quickshell-git)
 metapkgs+=(./sdata/dist-arch/illogical-impulse-bibata-modern-classic-bin)
+metapkgs+=(./sdata/dist-arch/illogical-impulse-denuvowo)
 
 for i in "${metapkgs[@]}"; do
   metainstallflags="--needed"
